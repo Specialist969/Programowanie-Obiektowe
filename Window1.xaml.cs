8a8f69e74818972e0504dc7ae1f23513070a22ff
Paweł Musiał <Pawel.Musial@microsoft.wsei.edu.pl>
2022-07-19 21:44:57 +0200

    tabela

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using MySqlConnector;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace ProgramowanieObiektowe
 {
@@ -25,9 +28,20 @@ namespace ProgramowanieObiektowe
             InitializeComponent();
         }
 
+        string sql = "SELECT * FROM Filmy";
         private void TabelaDuża_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-           
+        {// Wykonaj polecenie języka SQL na danych połączeniu
+           using (MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql))
+            {
+                DataTable dt = new DataTable();
+                // Pobierz dane i zapisz w strukturze DataTable
+                MySql.Data.MySqlClient.MySqlDataAdapter adapter = new MySql.Data.MySqlClient.MySqlDataAdapter(cmd);
+                adapter.Fill(dt);
+                //wpisz dane do kontrolki DATAGRID
+                _ = dt.DefaultView;
+
+
+            }
         }
 
         private void TabelaMała_SelectionChanged(object sender, SelectionChangedEventArgs e)
