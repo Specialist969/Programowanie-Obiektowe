d1e7c8742992bf6b86f9168cec19841f2f72c832
Paweł Musiał <Pawel.Musial@microsoft.wsei.edu.pl>
2022-07-19 21:53:52 +0200

    tabelka+

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -29,20 +29,12 @@ namespace ProgramowanieObiektowe
         }
 
         string sql = "SELECT * FROM Filmy";
-        private void TabelaDuża_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {// Wykonaj polecenie języka SQL na danych połączeniu
-           using (MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql))
-            {
-                DataTable dt = new DataTable();
-                // Pobierz dane i zapisz w strukturze DataTable
-                MySql.Data.MySqlClient.MySqlDataAdapter adapter = new MySql.Data.MySqlClient.MySqlDataAdapter(cmd);
-                adapter.Fill(dt);
-                //wpisz dane do kontrolki DATAGRID
-                _ = dt.DefaultView;
 
+        private void TabelaDuża_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
 
-            }
         }
+
 
         private void TabelaMała_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -51,8 +43,21 @@ namespace ProgramowanieObiektowe
 
         public void PobierzDane()
         {
-            string slq = "SELECT * FROM Pracownicy";
+            // Wykonaj polecenie języka SQL na danych połączeniu
+            using (MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql))
+            {
+                DataTable dt = new DataTable();
+                // Pobierz dane i zapisz w strukturze DataTable
+                MySql.Data.MySqlClient.MySqlDataAdapter adapter = new MySql.Data.MySqlClient.MySqlDataAdapter(cmd);
+                adapter.Fill(dt);
+                //wpisz dane do kontrolki DATAGRID
+                TabelaDuża.ItemsSource = dt.DefaultView;
+
+
+            }
         }
+
+        
     }
 }
 
